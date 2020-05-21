fee6eb301382f80c5c456b408548af7e8c757dbf
Megan Gomes <megan.gomes25@gmail.com>
2020-05-21 21:23:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else 
+      return false
   end
 
   def question?
-
+    if self.end_with("?")
   end
 
   def exclamation?
-
+    if self.end_with("!")
   end
 
   def count_sentences
-
+binding.pry
   end
+
 end
